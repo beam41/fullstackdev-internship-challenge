c092bed85cf45cc2f2e74c022c11030dcf2ce393
beam41 <beam_baimon@hotmail.com>
2020-01-28 21:41:22 +0700

    Add things to use

---
+++ exercise/vending-machine/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // make it easier to add new things to multiline array
+  trailingComma: "all",
+  // 80 is too tight
+  printWidth: 120,
+};
---
+++ exercise/vending-machine/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./App.module.scss";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function App() {
+  return <div className={styles.app}></div>;
+}
+
+export default App;
