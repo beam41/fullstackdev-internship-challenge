eabd1c48459b2eabac36cd908b4fb270cfd6cecf
beam41 <beam_baimon@hotmail.com>
2020-01-30 22:58:55 +0700

    remake coin display displaying method
    and add coin pick animation

--- exercise/vending-machine/src/components/popup/coin-printer/coin-printer.js
+++ exercise/vending-machine/src/components/popup/coin-printer/coin-printer.js
@@ -17,67 +17,31 @@ export default class CoinPrinter extends Component {
     coins: PropTypes.object,
   };
 
-  canvas = React.createRef();
-
-  constructor() {
-    super();
-    /*
-     * this state, eventListener, and componentDidMount
-     * exist to achived the same thing
-     * to set height of warpper box to == its width
-     * that cannot directly set in css.
-     * Pretty inefficient but it does the job.
-     */
-    this.state = {
-      clientWidth: 0,
-      clientHeight: 0,
-    };
-    window.addEventListener("resize", () => {
-      this.setState({ clientWidth: this.canvas.current.clientWidth, clientHeight: this.canvas.current.clientHeight });
-    });
-  }
-
-  componentDidMount() {
-    this.setState({ clientWidth: this.canvas.current.clientWidth, clientHeight: this.canvas.current.clientHeight });
-  }
-
-  componentDidUpdate() {
-    if (
-      this.state.clientWidth !== this.canvas.current.clientWidth ||
-      this.state.clientHeight !== this.canvas.current.clientHeight
-    )
-      this.setState({ clientWidth: this.canvas.current.clientWidth, clientHeight: this.canvas.current.clientHeight });
-  }
-
   render() {
     return (
-      <div ref={this.canvas} className={styles.canvas}>
-        {this.canvas.current &&
-          coinPrintloop(this.props.coins, this.state.clientWidth, this.state.clientHeight).map((ele, index) => {
-            return (
-              // Randomize position and rotation of coins
-              <span
-                style={{
-                  transform: `translate(${Math.random() * this.state.clientWidth * 0.85 - 5}px, ${Math.random() *
-                    this.state.clientHeight *
-                    0.75 -
-                    10}px)
-                              scale(0.5) 
+      <div className={styles.canvas}>
+        {coinPrintloop(this.props.coins).map((ele, index) => {
+          return (
+            // Randomize position and rotation of coins
+            <span
+              style={{
+                transform: `translate(${Math.random() * 50 - 25}px, ${Math.random() * 50 - 25}px)
+                              scale(0.5)
                               rotate(${Math.random() * 90 - 45}deg) `,
-                }}
-                className={styles.coin}
-                key={index}
-              >
-                {ele}
-              </span>
-            );
-          })}
+              }}
+              className={styles.coin}
+              key={index}
+            >
+              {ele}
+            </span>
+          );
+        })}
       </div>
     );
   }
 }
 
-function coinPrintloop(coins, width, height) {
+function coinPrintloop(coins) {
   let jsxArr = [];
   let key = 0;
   // individual
@@ -116,5 +80,14 @@ function coinPrintloop(coins, width, height) {
     key++;
   }
 
+  scrambleArr(jsxArr);
+
   return jsxArr;
+}
+
+function scrambleArr(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
