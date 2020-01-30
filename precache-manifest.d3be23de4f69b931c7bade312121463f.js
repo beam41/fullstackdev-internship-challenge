7ca48b59d2ae0b441438283573e4478d8c08ba2d
beam41 <beam_baimon@hotmail.com>
2020-01-31 00:32:37 +0700

    Updates

---
+++ precache-manifest.d3be23de4f69b931c7bade312121463f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387b7244dbc5c57be6de5c13fe6099fc",
+    "url": "/internship-challenge/index.html"
+  },
+  {
+    "revision": "4b39bd523c40f830d1ae",
+    "url": "/internship-challenge/static/css/main.38851faa.chunk.css"
+  },
+  {
+    "revision": "1bfa0cf2c9a529ecc79f",
+    "url": "/internship-challenge/static/js/2.865c838e.chunk.js"
+  },
+  {
+    "revision": "836687f75dbf024cab882ad1c8c0e52b",
+    "url": "/internship-challenge/static/js/2.865c838e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b39bd523c40f830d1ae",
+    "url": "/internship-challenge/static/js/main.a7004d31.chunk.js"
+  },
+  {
+    "revision": "af0e59a6977b65e224a6",
+    "url": "/internship-challenge/static/js/runtime-main.ac1abc9e.js"
+  }
+]);
